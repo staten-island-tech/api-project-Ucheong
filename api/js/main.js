7969a69aea0ce6fdcf85a813a4e6964a0f262844
Ucheong <ucheongsou@gmail.com>
2023-01-06 10:14:54 -0500

    vite

--- api/js/main.js
+++ api/js/main.js
@@ -1,23 +1,13 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+import "../styles/style.css";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
+const URL = "";
+async function getData(URL) {
+  try {
+    const respone = await fetch(URL);
+    const data = await respone.json();
+    document.getElementById("lol").textContent = data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);

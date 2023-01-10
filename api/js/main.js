b7ce6e5e0de705de05587143e1d054952a6d407e
Ucheong <ucheongsou@gmail.com>
2023-01-10 10:15:31 -0500

    api

--- api/js/main.js
+++ api/js/main.js
@@ -1,13 +1,23 @@
 import "../styles/style.css";
 
-const URL = "";
+const URL = "http://api.chartlyrics.com/GetLyric";
 async function getData(URL) {
   try {
-    const respone = await fetch(URL);
-    const data = await respone.json();
-    document.getElementById("").textContent = data;
+    const responses = await fetch(URL);
+    const data = await responses.json();
+    document.getElementById(".respone").textContent = data.Lyric;
   } catch (error) {
     console.log(error);
   }
 }
 getData(URL);
+
+document.querySelector(".light").addEventListener("click", function () {
+  document.body.classList.add("light");
+  document.body.classList.remove("dark");
+});
+
+document.querySelector(".dark").addEventListener("click", function () {
+  document.body.classList.add("dark");
+  document.body.classList.remove("light");
+});

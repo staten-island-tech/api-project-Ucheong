f9c2cd883c41a4f15742ad849bf480173e70e253
Ucheong <ucheongsou@gmail.com>
2023-01-17 19:36:54 -0500

    api

--- api/js/main.js
+++ api/js/main.js
@@ -3,17 +3,17 @@ import "./dom";
 import { DOMSelectors } from "./dom";
 import "./link";
 
-const URL = "";
-async function getData(URL) {
-  try {
-    const responses = await fetch(URL);
-    const data = await responses.json();
-    document.getElementById("response").textContent = data.cards.name;
-  } catch (error) {
-    console.log(error);
-  }
-}
-getData(URL);
+// const URL = "";
+// async function getData(URL) {
+//   try {
+//     const responses = await fetch(URL);
+//     const data = await responses.json();
+//     document.getElementById("response").textContent = data.cards.name;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// getData(URL);
 
 document.querySelector(".light").addEventListener("click", function () {
   document.body.classList.add("light");

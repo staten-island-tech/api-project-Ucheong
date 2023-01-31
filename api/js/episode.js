16db1eb6870bf14118286846005ba3f5705bee89
Ucheong <ucheongsou@gmail.com>
2023-01-30 21:35:37 -0500

    api

--- api/js/episode.js
+++ api/js/episode.js
@@ -6,49 +6,38 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
 
-const episodes1 = "https://rickandmortyapi.com/api/episode";
-const episodes2 = "https://rickandmortyapi.com/api/episode?page=2";
-const episodes3 = "https://rickandmortyapi.com/api/episode?page=3";
-
 document.querySelector(".episodes").addEventListener("click", function () {
   DOMSelectors.label.innerHTML = `Episode (Type it in S00E00 format!)`;
   DOMSelectors.display.innerHTML = ``;
   episodecard();
 });
 
-async function getEpisode() {
-  const responses = await fetch(episodes1);
-  const data = await responses.json();
-  return data;
+let episodes = [];
+
+async function episodecaller() {
+  episodes = [];
+  for (let i = 1; i <= 3; i++) {
+    let page1 = await fetch(
+      `https://rickandmortyapi.com/api/episode?page=${i}`
+    );
+    episodes.push(await page1.json());
+  }
 }
 
-// const data = async () => {
-//   try {
-//     const responses = await Promise.all(
-//       episodes.map((episode) => fetch(episode).then((res) => res.json()))
-//     );
-//     console.log(responses);
-//   } catch (error) {
-//     DOMSelectors.display.insertAdjacentHTML(
-//       "afterbegin",
-//       `<p class="error"> NOT FOUND </p>`
-//     );
-//   }
-// };
-// data();
-
 async function episodecard() {
-  let episode = await getEpisode();
-  episode.results.forEach((bob) => {
-    DOMSelectors.display.insertAdjacentHTML(
-      "afterbegin",
-      `<div class="display-card3" data-aos="fade-down">
+  await episodecaller();
+  episodes.forEach((episode) => {
+    episode.results.forEach((bob) => {
+      DOMSelectors.display.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="display-card3" data-aos="fade-down">
       <h2 class="name">Name: ${bob.name}</h2>
       <h2 class="date">Air Date: ${bob.air_date}</h2>
       <h2 class="episode">Episode: ${bob.episode}</h2>
       <button class="people"> Characters in the Episode </button>
       </div>`
-    );
+      );
+    });
   });
 }
 
@@ -61,9 +50,9 @@ async function episodecard() {
 async function init3() {
   try {
     DOMSelectors.display.innerHTML = ``;
-    let episode = await getEpisode();
+    await episodecaller();
     let e = DOMSelectors.input.value;
-    let filtered = episode.results.filter(
+    let filtered = episodes.results.filter(
       (lol) => lol.episode.includes(`${e}`) || lol.episode === `${e}`
     );
     filtered.map((bob) => {
--- api/js/location.js
+++ api/js/location.js
@@ -6,7 +6,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
 
-const location = "https://rickandmortyapi.com/api/location";
+// const location = "https://rickandmortyapi.com/api/location";
 
 document.querySelector(".location").addEventListener("click", function () {
   DOMSelectors.label.innerHTML = ``;
@@ -19,31 +19,58 @@ document.querySelector(".location").addEventListener("click", function () {
 //   document.body.innerHTML = ``;
 // });
 
-async function getLocation() {
-  const responses = await fetch(location);
-  const data = await responses.json();
-  return data;
+let locations = [];
+
+async function locationcaller() {
+  locations = [];
+  for (let i = 1; i <= 7; i++) {
+    let page1 = await fetch(
+      `https://rickandmortyapi.com/api/location?page=${i}`
+    );
+    locations.push(await page1.json());
+  }
 }
 
+// async function getLocation() {
+//   const responses = await fetch(location);
+//   const data = await responses.json();
+//   return data;
+// }
+
 async function locationcard() {
-  let locations = await getLocation();
-  locations.results.forEach((bob) => {
-    DOMSelectors.display.insertAdjacentHTML(
-      "afterbegin",
-      `<div class="display-card2" data-aos="fade-down">
+  await locationcaller();
+  locations.forEach((location) => {
+    location.results.forEach((bob) => {
+      DOMSelectors.display.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="display-card2" data-aos="fade-down">
         <h2 class="name"> Name: ${bob.name}</h2>
         <h2 class="type"> Type: ${bob.type}</h2>
         <h2 class="dimension"> Dimension: ${bob.dimension}</h2>
         <button class="residents"> Residents </button>
         </div>`
-    );
+      );
+    });
   });
 }
+
+//   locations.results.forEach((bob) => {
+//     DOMSelectors.display.insertAdjacentHTML(
+//       "afterbegin",
+//       `<div class="display-card2" data-aos="fade-down">
+//         <h2 class="name"> Name: ${bob.name}</h2>
+//         <h2 class="type"> Type: ${bob.type}</h2>
+//         <h2 class="dimension"> Dimension: ${bob.dimension}</h2>
+//         <button class="residents"> Residents </button>
+//         </div>`
+//     );
+//   });
+// }
 
 async function init2() {
   try {
     DOMSelectors.display.innerHTML = ``;
-    let locations = await getLocation();
+    await locationcaller();
     let place = DOMSelectors.input.value;
     let filtered = locations.results.filter(
       (lol) => lol.name.includes(`${place}`) || lol.name === `${place}`
